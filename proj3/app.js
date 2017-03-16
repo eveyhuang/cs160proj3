d2cffc44fd23d0a7a3741e71873ed060f2514d48
Harry Sutcliffe <hsutcliffe@berkeley.edu>
2017-03-15 21:45:12 -0700

    with web URL

--- proj3/app.js
+++ proj3/app.js
@@ -25,13 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-
-// //Placeholder route
-// app.get('/', function (req, res) {
-//   res.end('Hi there!')
-// })
-// app.listen(3000)
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
