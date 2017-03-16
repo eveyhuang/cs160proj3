354ff2d1f98e9a5b073601081bc442f66d805001
Jiaxin Huang <evie@Jiaxins-MacBook-Pro.local>
2017-03-16 00:02:36 -0700

    first recipe box

--- proj3/app.js
+++ proj3/app.js
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
